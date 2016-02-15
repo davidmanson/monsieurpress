388a89bbc7396a2751705be62c0dd3b55be61a7f
David Manson <david.manson@me.com>
2016-02-15 16:37:35 +0100

    cleaner function.php file

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  ************************************/
 
 
-// LOAD LIBS
+// Load dependencies
 require_once( 'inc/helpers.php' );
 require_once( 'inc/custom-post-type.php' );
 
@@ -15,16 +15,10 @@ require_once( 'inc/custom-post-type.php' );
 Let's get everything up and running.
 *************************************/
 function theme_init() {
-
   add_action( 'init', 'head_cleanup' );
   add_action( 'init', 'theme_support' );
-  add_action( 'widgets_init', 'theme_sidebars' );
-
-  add_filter( 'the_content', 'remove_ptags_on_images' );
-  add_filter( 'excerpt_more', 'custom_excerpt_more' );
-
+  add_action( 'init', 'theme_filters');
 }
-
 add_action( 'after_setup_theme', 'theme_init' );
 
 
@@ -38,20 +32,12 @@ function theme_support() {
 	add_theme_support( 'menus' );
     add_theme_support( 'title-tag' );
 
-	register_nav_menus(
-		array(
-			'main-nav' => __( 'The Main Menu', 'mrpress' ),   // main nav in header
-			'footer-links' => __( 'Footer Links', 'mrpress' ) // secondary nav in footer
-		)
-	);
-
 	// Enable support for HTML5 markup.
 	add_theme_support( 'html5', array(
 		'comment-list',
 		'search-form',
 		'comment-form'
 	) );
-
 }
 
 
@@ -76,6 +62,18 @@ add_image_size( 'mrpress-medium', 700, round(700/$thumb_realisation_ratio), true
 
 
 /************************************
+ Menu
+*************************************/
+function theme_menu() {
+    register_nav_menus(array(
+		'main-nav' => __( 'The Main Menu', 'mrpress' ),   // main nav in header
+		'footer-links' => __( 'Footer Links', 'mrpress' ) // secondary nav in footer
+	));
+}
+
+
+
+/************************************
  Sidebar
 *************************************/
 function theme_sidebars() {
@@ -86,7 +84,18 @@ function theme_sidebars() {
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 	));
-} 
+}
+add_action( 'widgets_init', 'theme_sidebars' );
+
+
+
+/************************************
+ Theme filters
+*************************************/
+function theme_filters(){
+    add_filter( 'the_content', 'remove_ptags_on_images' );
+    add_filter( 'excerpt_more', 'custom_excerpt_more' );
+}
 
 
 
@@ -95,22 +104,20 @@ function theme_sidebars() {
 *************************************/
 function front_assets_load() {
     if (is_admin()) return;
-    
+
     /* deregister jQuery in header & register it in footer & deregister wp-embed */
     wp_deregister_script( 'wp-embed' );
     wp_deregister_script( 'jquery' );
     wp_deregister_script( 'jquery-migrate');
     wp_register_script( 'jquery', includes_url( '/js/jquery/jquery.js' ), false, NULL, true );
-    
+
     /* Enqueue theme script & style */
     wp_enqueue_style( 'mrpress-stylesheet', get_stylesheet_uri()  );
     wp_enqueue_script( 'site-js', get_template_directory_uri() . '/js/scripts.js', array(), '20160207', true);
-    
+
     /* Enqueue comment-reply script if needed */
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-
-
 add_action('wp_enqueue_scripts', 'front_assets_load');
