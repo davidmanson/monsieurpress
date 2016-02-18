2ae804725f9bcf2d0e3335f73fb7488f7879194e
David Manson <david.manson@me.com>
2016-02-18 10:02:14 +0100

    remove useless log in js

--- js/scripts.js
+++ js/scripts.js
@@ -5,42 +5,40 @@
 
 
 ;(function(){
-    
+
     // Variables declarations
     var $body,
         $menuButtons,
         $mobileMenu,
         toggleMenu,
         onLoad;
-    
-    
-    
+
+
     /*********************
-     * On document ready 
+     * On document ready
      *********************/
     onLoad = function(){
-        
+
         // Getting needed DOM
         $body        = document.getElementsByTagName('body')[0];
         $menuButtons = document.getElementsByClassName('js-menu-toggle');
 
-        
+
         // Binding buttons to toggle
         var i = $menuButtons.length;
         while(i--) $menuButtons[i].addEventListener('click', toggleMenu);
-        
+
     }
-    
-    
+
+
     /**********************
      * Toggle mobile menu
      **********************/
     toggleMenu = function(){
-        console.log($body);
         $body.classList.toggle("is-menu-on");
     }
-    
-    
+
+
     // Binding page load
     document.addEventListener('DOMContentLoaded', onLoad);
 })();
@@ -55,11 +53,11 @@
 ************************************************/
 /*
 jQuery(document).ready(function($) {
-    
+
     $('.js-menu-toggle').on('click', function(){
         $('.site-nav').slideToggle();
         $('body').toggleClass('menu-on');
     });
-    
+
 });
 */
