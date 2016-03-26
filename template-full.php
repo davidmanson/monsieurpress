ca48c5511f604d78bb187cd030d051b846dbfe18
David Manson <david.manson@me.com>
2016-03-27 00:04:34 +0100

    full template now has container

--- template-full.php
+++ template-full.php
@@ -6,10 +6,10 @@
 
 <?php get_header(); ?>
 
-<?php
-while (have_posts()) : the_post();
-    the_content();
-endwhile;
-?>
+<?php while (have_posts()) : the_post(); ?>
+    <div class="l-container">
+        <?php the_content(); ?>
+    </div>
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
